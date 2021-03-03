bb0d980a49e845d6eb4721a7bae726fec101f72b
Dag Gebreselasse <dgebreselasse@gmail.com>
2021-03-02 23:34:13 -0600

    added a function that opens optical character recognition site and loops through the cropped bar chart images uploads them to the site and extracts rent estiamte text and prints it out to cmd line.

--- rent.py
+++ rent.py
@@ -70,6 +70,8 @@ size1 = (85,55,185,300)
 size2 = (185,55,255,300)
 size3 = (255,55,330,300)
 size4 = (330,55,430,300)
+imageList = []
+
 
 for strAddress in addresses:
     for bed in range(4):
@@ -88,16 +90,38 @@ for strAddress in addresses:
         cropImg2 = resizeImg.crop(size2)
         cropImg3 = resizeImg.crop(size3)
         cropImg4 = resizeImg.crop(size4)
+
         cropImg1.save("./charts/"+strAddress.replace(" ", "").replace(",", "")+"-"+str(bed+1)+"-1"+".png")
+        imageList.append("/charts/"+strAddress.replace(" ", "").replace(",", "")+"-"+str(bed+1)+"-1"+".png")
+
         cropImg2.save("./charts/"+strAddress.replace(" ", "").replace(",", "")+"-"+str(bed+1)+"-2"+".png")
+        imageList.append("/charts/"+strAddress.replace(" ", "").replace(",", "")+"-"+str(bed+1)+"-2"+".png")
+
         cropImg3.save("./charts/"+strAddress.replace(" ", "").replace(",", "")+"-"+str(bed+1)+"-3"+".png")
+        imageList.append("/charts/"+strAddress.replace(" ", "").replace(",", "")+"-"+str(bed+1)+"-3"+".png")
+
         cropImg4.save("./charts/"+strAddress.replace(" ", "").replace(",", "")+"-"+str(bed+1)+"-4"+".png")
+        imageList.append("/charts/"+strAddress.replace(" ", "").replace(",", "")+"-"+str(bed+1)+"-4"+".png")
 
 
 
 
-# driver.get("https://brandfolder.com/workbench/extract-text-from-image")
-# time.sleep(5)
+driver.get("https://brandfolder.com/workbench/extract-text-from-image")
+
+
+
+for imgItem in imageList:
+    time.sleep(5)
+    upload = driver.find_element_by_class_name("fsp-drop-pane__input")
+    upload.clear()
+    upload.send_keys(os.getcwd() + imgItem)
+    time.sleep(5)
+    textEl = driver.find_element_by_id("extracted_text")
+    text = textEl.text
+    print(text)
+    driver.refresh()
+
+
 # upload = driver.find_element_by_class_name("fsp-drop-pane__input")
 # upload.send_keys(os.getcwd() + "/a.png")
 # time.sleep(5)
